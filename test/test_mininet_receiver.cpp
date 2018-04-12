0806766e98d4aa7e17afe25d69afa502858e0c3a
Alston Tang <tanghaomo@gmail.com>
2018-04-12 01:06:43 -0700

    Mininet speed test executable

--- test/test_mininet_receiver.cpp
+++ test/test_mininet_receiver.cpp
@@ -2,3 +2,30 @@
 // Created by tang on 4/11/18.
 //
 
+#include <cstring>
+#include <zconf.h>
+#include "../NORM/NormReceiver.h"
+
+using namespace DCCast;
+
+#ifndef SENDER_RATE
+#define SENDER_RATE 30000
+#endif
+
+int main() {
+    NormReceiver receiver(0, 8888);
+    timespec start_time = {}, end_time = {};
+    if (!clock_gettime(CLOCK_REALTIME, &start_time)) {
+        std::cerr << "Can not get time" << std::endl;
+    }
+    for (int i = 0; i < 120; i++) {
+        std::cout << receiver.get_progress() << " bytes has been sent" << std::endl;
+        sleep(1);
+    }
+    if (!clock_gettime(CLOCK_REALTIME, &end_time)) {
+        std::cerr << "Can not get time" << std::endl;
+    }
+    double diff = end_time.tv_sec - start_time.tv_sec +
+            static_cast<double >(end_time.tv_nsec - start_time.tv_nsec) / 1000000000;
+    std::cout << "Receiving rate: " << receiver.get_progress() / diff;
+}
--- test/test_mininet_sender.cpp
+++ test/test_mininet_sender.cpp
@@ -2,4 +2,23 @@
 // Created by tang on 4/11/18.
 //
 
+#include <cstring>
+#include <zconf.h>
+#include "../NORM/NormSender.h"
 
+using namespace DCCast;
+
+#ifndef SENDER_RATE
+#define SENDER_RATE 30000
+#endif
+
+int main() {
+    auto *data = new char[102400];
+    memset(data, 'a', 102400);
+    std::cout << "Sending rate: " << SENDER_RATE << std::endl;
+    NormSender sender(0, "10.0.0.2", 8888, SENDER_RATE, data, 102400);
+    for (int i = 0; i < 120; i++) {
+        std::cout << sender.get_progress() << " bytes has been sent" << std::endl;
+        sleep(1);
+    }
+}
